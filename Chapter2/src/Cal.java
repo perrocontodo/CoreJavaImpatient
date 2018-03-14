7fed8fe545c6523d93f2671efecb949133c7d47f
Pormu <pormudanzavendotodo@gmail.com>
2018-03-14 23:09:22 +1100

    Chapter 2. Exercise 1

---
+++ Chapter2/src/Cal.java
@@ -0,0 +1,36 @@
+/* 1. Change the calendar printing program so it starts the week on a Sunday.
+ * Also make it print a newline at the end (but only one).
+ */
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+public class Cal {
+
+    public static void printCalendar(String[] args) {
+        LocalDate date = LocalDate.now().withDayOfMonth(1);
+        int month;
+        if (args.length >= 2) {
+            month = Integer.parseInt(args[0]);
+            int year = Integer.parseInt(args[1]);
+            date = LocalDate.of(year, month, 1);
+        } else {
+            month = date.getMonthValue();
+        }
+
+        System.out.println(" Sun Mon Tue Wed Thu Fri Sat");
+        DayOfWeek weekday = date.getDayOfWeek();
+        int value = weekday.plus(1L).getValue();
+        for (int i = 1; i < value; i++)
+            System.out.print("    ");
+        while (date.getMonthValue() == month) {
+            System.out.printf("%4d", date.getDayOfMonth());
+            date = date.plusDays(1);
+            if (date.getDayOfWeek().plus(1L).getValue() == 1)
+                System.out.println();
+        }
+        if (date.getDayOfWeek().getValue() != 1)
+            System.out.println();
+        System.out.println();
+    }
+}
---
+++ Chapter2/src/Chapter2.java
@@ -0,0 +1,135 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Chapter2 {
+
+    public static void main(String[] args) {
+
+        System.out.print("Exercise number? ");
+        Scanner in = new Scanner(System.in);
+        int exercise = 0;
+        try {
+            exercise = in.nextInt();
+        } catch (InputMismatchException e) {
+            printUsage();
+        }
+
+        if (exercise < 1 || exercise > 17) {
+            printUsage();
+        } else {
+            switch (exercise) {
+                case 1:
+                    System.out.print("Type month and year, as \"3 2018\": ");
+                    Scanner arguments = new Scanner(System.in);
+                    Cal.printCalendar(arguments.nextLine().split(" "));
+                    break;
+                case 2:
+                    exercise2();
+                    break;
+                case 3:
+                    exercise3();
+                    break;
+                case 4:
+                    exercise4();
+                    break;
+                case 5:
+                    exercise5();
+                    break;
+                case 6:
+                    exercise6();
+                    break;
+                case 7:
+                    exercise7();
+                    break;
+                case 8:
+                    exercise8();
+                    break;
+                case 9:
+                    exercise9();
+                    break;
+                case 10:
+                    exercise10();
+                    break;
+                case 11:
+                    exercise11();
+                    break;
+                case 12:
+                    exercise12();
+                    break;
+                case 13:
+                    exercise13();
+                    break;
+                case 14:
+                    exercise14();
+                    break;
+                case 15:
+                    exercise15();
+                    break;
+                case 16:
+                    exercise16();
+                    break;
+                case 17:
+                    exercise17();
+                    break;
+                default:
+                    printUsage();
+                    break;
+
+            }
+        }
+    }
+
+    private static void exercise2() {
+    }
+
+    private static void exercise3() {
+    }
+
+    private static void exercise4() {
+    }
+
+    private static void exercise5() {
+    }
+
+    private static void exercise6() {
+    }
+
+    private static void exercise7() {
+    }
+
+    private static void exercise8() {
+    }
+
+    private static void exercise9() {
+    }
+
+    private static void exercise10() {
+    }
+
+    private static void exercise11() {
+    }
+
+    private static void exercise12() {
+    }
+
+    private static void exercise13() {
+    }
+
+    private static void exercise14() {
+    }
+
+    private static void exercise15() {
+    }
+
+    private static void exercise16() {
+    }
+
+    private static void exercise17() {
+    }
+
+    private static void printUsage() {
+        System.out.println("Exercise should be a number between 1 and 17");
+        System.exit(-1);
+    }
+}
+
